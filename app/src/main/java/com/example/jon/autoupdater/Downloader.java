2ae597f8502d184f8a7562c4ce1d142417e57def
Scott Erickson <scott@mysnowcloud.com>
2019-02-20 20:27:23 -0800

    first commit

---
+++ app/src/main/java/com/example/jon/autoupdater/Downloader.java
@@ -0,0 +1,22 @@
+package com.example.jon.autoupdater;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class Downloader {
+    protected void onCreate(Bundle savedInstanceState) {
+
+    }
+
+
+}
---
+++ app/src/main/java/com/example/jon/autoupdater/MainActivity.java
@@ -0,0 +1,143 @@
+package com.example.jon.autoupdater;
+
+import android.app.DownloadManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.Uri;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String dwnUrl = new String("https://www.dropbox.com/s/dbaelzvc37xby7w/app-debug.apk?dl=0");
+        this.update(dwnUrl);
+//        String appUrl = "https://www.dropbox.com/s/dbaelzvc37xby7w/app-debug.apk?dl=0";
+//        String downFile = this.doInBackground(appUrl);
+//        this.onPostExecute(downFile);
+
+//        Button doButton = (Button) findViewById(R.id.button);
+//        doButton.setOnClickListener(new View.OnClickListener() {
+//            public void onClick (View v) {
+//                MainActivity.caller(appPath);
+//            }
+//        });
+    }
+
+//    public static void caller(String path){
+//        this.onPostExecute(path);
+//        return;
+//    }
+//
+
+    protected String doInBackground(String... sUrl) {
+        String path = "/SD card/AutoUpdater.apk";
+        try {
+            URL url = new URL(sUrl[0]);
+            URLConnection connection = url.openConnection();
+            connection.connect();
+
+            int fileLength = connection.getContentLength();
+
+            // download the file
+            InputStream input = new BufferedInputStream(url.openStream());
+            OutputStream output = new FileOutputStream(path);
+
+            byte data[] = new byte[1024];
+            long total = 0;
+            int count;
+            while ((count = input.read(data)) != -1) {
+                total += count;
+//                publishProgress((int) (total * 100 / fileLength));
+                output.write(data, 0, count);
+            }
+
+            output.flush();
+            output.close();
+            input.close();
+        } catch (Exception e) {
+            Log.d("AutoUpdater", e.getMessage());
+        }
+        return path;
+    }
+
+    // begin installation by opening the resulting file
+
+    protected void onPostExecute(String path) {
+        Intent i = new Intent();
+        i.setAction(Intent.ACTION_VIEW);
+        i.setDataAndType(Uri.fromFile(new File(path)), "application/vnd.android.package-archive");
+        Log.d("Lofting", "About to install new .apk");
+        this.startActivity(i);
+    }
+
+    public void update(String url){
+        //get destination to update file and set Uri
+        //TODO: First I wanted to store my update .apk file on internal storage for my app but apparently android does not allow you to open and install
+        //aplication with existing package from there. So for me, alternative solution is Download directory in external storage. If there is better
+        //solution, please inform us in comment
+        String destination = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS) + "/";
+        String fileName = "AutoUpdater.apk";
+        destination += fileName;
+        final Uri uri = Uri.parse("file://" + destination);
+
+        //Delete update file if exists
+        File file = new File(destination);
+        if (file.exists())
+            //file.delete() - test this, I think sometimes it doesnt work
+            file.delete();
+
+        //get url of app on server
+//        String url = Main.this.getString(R.string.update_app_url);
+
+        //set downloadmanager
+        DownloadManager.Request request = new DownloadManager.Request(Uri.parse(url));
+//        request.setDescription(this.getString(R.string.notification_description));
+        request.setDescription(this.getString(R.string.notification_description));
+        request.setTitle(this.getString(R.string.app_name));
+
+        //set destination
+        request.setDestinationUri(uri);
+
+        // get download service and enqueue file
+        final DownloadManager manager = (DownloadManager) getSystemService(Context.DOWNLOAD_SERVICE);
+        final long downloadId = manager.enqueue(request);
+
+        //set BroadcastReceiver to install app when .apk is downloaded
+        BroadcastReceiver onComplete = new BroadcastReceiver() {
+            public void onReceive(Context ctxt, Intent intent) {
+                Intent install = new Intent(Intent.ACTION_VIEW);
+                install.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                install.setDataAndType(uri,
+                        manager.getMimeTypeForDownloadedFile(downloadId));
+                startActivity(install);
+
+                unregisterReceiver(this);
+                finish();
+            }
+        };
+        //register receiver for when .apk download is compete
+        registerReceiver(onComplete, new IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE));
+    }
+
+
+}
+
